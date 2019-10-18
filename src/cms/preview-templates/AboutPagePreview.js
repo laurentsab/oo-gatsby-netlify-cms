8254b979609c28f89ec2b9e67a25ebf72856c30a
Laurent Sabatier <lsabatier@clever-age.com>
2019-10-18 14:08:43 +0200

    Fix AboutPagePreview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import { AProposTemplate } from '../../templates/a-propos'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
+const AProposPreview = ({ entry, widgetFor }) => (
+  <AProposTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
 )
 
-AboutPagePreview.propTypes = {
+AProposPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default AboutPagePreview
+export default AProposPreview
